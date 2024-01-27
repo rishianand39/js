8ea3c0f1669d238e3c2e075ddf6a4a165c9a8cd2
rishianand39 <rishi.rn818@gmail.com>
2024-01-27 22:34:03 +0530

    repeat method

---
+++ newTerms.js
@@ -0,0 +1,8 @@
+// REPEAT()
+// repeat() function is a method available for strings. It is used to create a new string by repeating the original string a specified number of times. 
+
+let originalString = "Hello, ";
+let repeatedString = originalString.repeat(3);
+
+console.log(repeatedString);
+// Output: Hello, Hello, Hello,
