3bf25640b2b225faa42ce1e20ad6de38b36aa688
rishianand39 <rishi.rn818@gmail.com>
2024-06-01 23:55:33 +0530

    mimicked forEach

---
+++ forEach.polyfills.js
@@ -0,0 +1,20 @@
+// mimic forEach
+
+let array = [1, 2, 3, 4, 5];
+
+// array.forEach((element)=>{
+//     console.log(element*2)
+// })
+
+Array.prototype.forEach = null;
+
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function (callBack) {
+    for (let i = 0; i < array.length; i++) {
+      callBack(array[i], i);
+    }
+  };
+}
+array.forEach((element) => {
+  console.log(element * 2);
+});
